c1cb9c8c6908f58f9c41c6011e37c45cd8a31493
RatnaTriwish <ratna@triwish.io>
2023-03-17 13:14:40 +0530

    commit

---
+++ src/test/java/com/ratna/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratna.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
